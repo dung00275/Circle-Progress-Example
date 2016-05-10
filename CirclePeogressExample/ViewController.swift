6ee7b9d69a97bd9f2b3f5853064eaec7e8819ee0
Dung Vu <dung@hoang-phuongs-macbook-pro.local>
2016-05-10 21:29:27 +0700

    Initial Commit

---
+++ CirclePeogressExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CirclePeogressExample
+//
+//  Created by Hoang Phuong on 5/10/16.
+//  Copyright © 2016 Hoang Dung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CirclePeogressExampleTests/CirclePeogressExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  CirclePeogressExampleTests.swift
+//  CirclePeogressExampleTests
+//
+//  Created by Hoang Phuong on 5/10/16.
+//  Copyright © 2016 Hoang Dung. All rights reserved.
+//
+
+import XCTest
+@testable import CirclePeogressExample
+
+class CirclePeogressExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
